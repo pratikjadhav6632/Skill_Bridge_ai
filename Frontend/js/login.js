86ee3ce31d0478ff80a4cd9b564d8d8a1639ebd8
Pratik Jadhav <pratikjadhav6632@gmail.com>
2025-03-02 14:43:34 +0530

    ading firebase for login and signup page

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -18,3 +18,46 @@ signUpBtnLink.addEventListener('click', () => {
 signInBtnLink.addEventListener('click', () => {
     wrapper.classList.toggle('active');
 });
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCLK8WZHbOKKhe4WRwvXewqHolHOQjX39Y",
+    authDomain: "skill-bridge-d102b.firebaseapp.com",
+    projectId: "skill-bridge-d102b",
+    storageBucket: "skill-bridge-d102b.firebasestorage.app",
+    messagingSenderId: "321096933150",
+    appId: "1:321096933150:web:76532bb68c0a10c46bf30f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //input fields
+
+
+  //submit button 
+  const submit=document.getElementById('submit');
+  submit.addEventListener('click',function(event){
+    event.preventDefault();
+    const email=document.getElementById('email').value;
+    const password=document.getElementById('password').value;
+    const username=document.getElementById('username').value;
+    createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert("login SUccesfully");
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+  })
